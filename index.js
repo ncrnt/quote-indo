0b6f33f1208e4a6019993699215528691b64c336
Natsu <75367930+Natsu062@users.noreply.github.com>
2021-06-26 01:26:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ function Quotes(query) {
     validQuote = [
         "bucin",
         "galau",
-        "kehidupan"
+        "kehidupan",
+        "random"
     ]
 
     if (!query) throw new Error(`Masukkan Query!\nQuote yang tersedia:\n${validQuote.map((q, i) => `${i + 1}. ${q}`).join('\n')}\n`);
     if (query.toLowerCase() == 'bucin') {
-        const bucinQ = bucinQuotes[Math.floor(Math.random() * (bucinQuotes.length - 1))];
+        const bucinQ = bucinQuotes[Math.floor(Math.random() * (bucinQuotes.length))];
 
         return bucinQ;
     } else if (query.toLowerCase() == 'galau') {
-        const galauQ = galauQuotes[Math.floor(Math.random() * (galauQuotes.length - 1))];
+        const galauQ = galauQuotes[Math.floor(Math.random() * (galauQuotes.length))];
 
         return galauQ;
     } else if (query.toLowerCase() == 'kehidupan') {
-        const kehidupanQ = kehidupanQuotes[Math.floor(Math.random() * (kehidupanQuotes.length - 1))];
+        const kehidupanQ = kehidupanQuotes[Math.floor(Math.random() * (kehidupanQuotes.length))];
 
         return kehidupanQ;
+    } else if (query == 'random') {
+        const que = [
+            bucinQuotes,
+            galauQuotes,
+            kehidupanQuotes
+        ]
+
+        const randomQ = que[Math.floor(Math.random() * (que.length))]
+        const finalRandomQ = randomQ[Math.floor(Math.random() * (randomQ.length))]
+
+        return finalRandomQ;
     } else {
         // throw new Error(`Quote \"${query}\" tidak ditemukan!`)
         return console.log(`Quote \"${query}\" tidak ditemukan! \nQuote yang tersedia:\n${validQuote.map((q, i) => `${i + 1}. ${q}`).join('\n')}\n`)
